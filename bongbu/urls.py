c079954cc9655abe61e44dac81afbfa833539844
kwchoi8299 <kwchoi8299@gmail.com>
2023-05-06 11:10:47 +0900

    봉부 프로젝트 최초 커밋밋

---
+++ bongbu/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import base_views, question_views, answer_views
+
+app_name = 'bongbu'
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    # path('question/create/', views.question_create, name='question_create'),
+
+    # base_views.py
+    path('', base_views.index, name='index'),
+    path('<int:question_id>/', base_views.detail, name='detail'),
+
+    # question_views.py
+    path('question/create/', question_views.question_create, name='question_create'),
+
+    # answer_views.py
+    path('answer/create/<int:question_id>/', answer_views.answer_create, name='answer_create'),
+]
