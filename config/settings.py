22fad5e8a75708479ec58bfb1ed71282a5d111e9
kwchoi8299 <kwchoi8299@gmail.com>
2023-05-07 23:33:00 +0900

    host ip 변경

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-!2$di*x@t@r0bse(+)995(7n-lf_!7cxnutyn*u(s&y82$=7e-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['146.56.178.82']
-
+# ALLOWED_HOSTS = ['146.56.178.82']
+ALLOWED_HOSTS = ['10.0.0.38']
 
 # Application definition
 
