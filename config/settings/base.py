4354b2597a2aba6b297f5855044b4f1777a39bcd
kwchoi8299 <kwchoi8299@gmail.com>
2023-05-08 00:35:21 +0900

    setting.py 분리

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-!2$di*x@t@r0bse(+)995(7n-lf_!7cxnutyn*u(s&y82$=7e-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['146.56.178.82']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['146.56.178.82']
 # ALLOWED_HOSTS = ['10.0.0.38']
 
 # Application definition
---
+++ config/settings/prod.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = ['146.56.178.82']
